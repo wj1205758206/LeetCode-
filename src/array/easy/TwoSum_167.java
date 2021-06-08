97e15f3e800c055dc4d515c2a04709324ed86c6a
weijian <1205758206@qq.com>
2021-06-08 17:30:20 +0800

    有序数组中查找两数之和等于目标值，并返回这两个数在数组中的位置

---
+++ src/array/easy/TwoSum_167.java
@@ -0,0 +1,29 @@
+package array.medium;
+
+public class TwoSum_167 {
+    public static void main(String[] args) {
+        int[] numbers = {2,7,11,15};
+        int target = 9;
+        Solution solution = new Solution();
+        int[] indexs = solution.twoSum(numbers, target);
+        System.out.println(indexs[0] + "," + indexs[1]);
+
+    }
+
+    static class Solution {
+        public int[] twoSum(int[] numbers, int target) {
+            int left = 0;
+            int right = numbers.length - 1;
+            while (left < right) {
+                if (numbers[left] + numbers[right] == target)
+                    return new int[] {left + 1, right + 1};
+                else if (numbers[left] + numbers[right] < target) {
+                    left++;
+                }else if (numbers[left] + numbers[right] > target) {
+                    right--;
+                }
+            }
+            return new int[]{-1, -1};
+        }
+    }
+}
