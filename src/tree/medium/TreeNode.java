b2bb59e7db24126bf58684a7e37c1c81bec26862
weijian <1205758206@qq.com>
2021-06-21 23:30:27 +0800

    定义树的节点

---
+++ src/tree/medium/TreeNode.java
@@ -0,0 +1,23 @@
+package tree.medium;
+
+/**
+ * 定义树的节点
+ */
+public class TreeNode {
+    public int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
