3b7050dd7a682f0f8cc715a56e8c177810d487de
weijian <1205758206@qq.com>
2021-08-30 21:40:33 +0800

    最大子序和

--- src/dp/easy/MaxSubarray_53.java
+++ src/dp/easy/MaxSubarray_53.java
@@ -41,5 +41,41 @@ public class MaxSubarray_53 {
 
             return max;
         }
+
+        /**
+         * 方法二：动态规划，使用DP表，自底向上计算
+         *
+         * @param nums
+         * @return
+         */
+        public int maxSubArray2(int[] nums) {
+            /*首先定义dp数组的含义
+             * 这里不能定义dp数组为 nums[0..i] 中的「最大的子数组和」为 dp[i]
+             * 因为子数组一定是连续的，按照我们当前 dp 数组定义，并不能保证 nums[0..i] 中的最大子数组与 nums[i+1] 是相邻的，也就没办法从 dp[i] 推导出 dp[i+1]
+             * 而是定义为 以 nums[i] 为【结尾】的「最大子数组和」为 dp[i]。
+             * */
+            int[] dp = new int[nums.length];
+
+            /*基准条件
+             * 初始第一个元素就是自己为最大子数组和*/
+            dp[0] = nums[0];
+
+            /*循环遍历nums数组每一个元素，计算出每一个元素的状态转移，即计算出每一个以nums[i]元素结尾的最大子数组和，并保存在dp数组中*/
+            for (int i = 1; i < nums.length; i++) {
+                /*状态转移
+                 * dp[i] 有两种「选择」，
+                 * 要么与前面的相邻子数组连接，形成一个和更大的子数组；
+                 * 要么不与前面的子数组连接，自成一派作为起始，自己作为一个子数组。*/
+                dp[i] = Math.max(nums[i], nums[i] + dp[i - 1]);
+            }
+
+            /*dp数组中保存着以各个元素结尾的最大子数组和，所以返回最大值即为nums数组中最大子数组和*/
+            int result = Integer.MIN_VALUE;
+            for (int i = 0; i < dp.length; i++) {
+                result = Math.max(result, dp[i]);
+            }
+
+            return result;
+        }
     }
 }
