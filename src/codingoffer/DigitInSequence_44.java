11f3d2f84bdaf3e20ed113c85cc5880d6ae9c42b
weijian <1205758206@qq.com>
2021-09-24 01:22:45 +0800

    数字序列中某一位的数字

---
+++ src/codingoffer/DigitInSequence_44.java
@@ -0,0 +1,16 @@
+package codingoffer;
+
+/**
+ * 数字序列中某一位的数字
+ */
+public class DigitInSequence_44 {
+    public static void main(String[] args) {
+
+    }
+
+    class Solution {
+        public int findNthDigit(int n) {
+            return 0;
+        }
+    }
+}
